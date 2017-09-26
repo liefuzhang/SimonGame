28dbb4c27c73816d86054e2c36da9b0d0c704345
liefu zhang <liefuzhangnz@gmail.com>
2017-09-26 23:35:10 +1300

    some click hander code

--- 1.js
+++ 1.js
@@ -1,3 +1,35 @@
 $(document).ready(() => {
-    
+    setupClickHandler();
 });
+
+var game = {
+    series: [],
+    isStrict: false,
+    currentCount: 0
+};
+
+function setupClickHandler() {
+    $('#start-button').click(() => {
+        generateSeries();
+        startGame();
+    });
+
+    $('.plate-button').click(function () {
+        if ($(this)[0].classList.contains("unclickable")) {
+            return;
+        }
+        alert("button clicked");
+    });
+}
+
+function generateSeries() {
+    game.series = [];
+    for (var i = 0; i < 20; i++) {
+        var rand = Math.floor(Math.random() * 4 + 1)
+        game.series.push(rand);
+    }
+}
+
+function startGame() {
+
+}
