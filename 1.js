1d6298c113a18b43754b157af692d051453232c1
liefu zhang <liefuzhangnz@gmail.com>
2017-09-29 00:00:13 +1300

    improved game logic

--- 1.js
+++ 1.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     setupClickHandler();
+    gameInit = Object.assign({}, game);
     audios[0].volume = 0.02;
 });
 
@@ -7,24 +8,51 @@ var game = {
     series: [],
     isStrict: false,
     currentCount: 0,
+    expectedIndex: 0,
     timeoutHandler: 0
 };
 
+var gameInit = {};
+
 var audios = [new Audio('res/error.mp3'),
-    new Audio('res/simonSound1.mp3'),
-    new Audio('res/simonSound2.mp3'),
-    new Audio('res/simonSound3.mp3'),
-    new Audio('res/simonSound4.mp3')];
+new Audio('res/simonSound1.mp3'),
+new Audio('res/simonSound2.mp3'),
+new Audio('res/simonSound3.mp3'),
+new Audio('res/simonSound4.mp3')];
 
 function setupClickHandler() {
     $('#start-button').click(() => {
-        generateSeries();
         startGame();
     });
 
     $('.plate-button').click(function () {
-
+        var id = $(this).attr("id").match(/\d+/)[0];
+        onButtonClick(Number(id));
     });
+}
+
+function onButtonClick(id) {
+    if (id === game.series[game.expectedIndex]) {
+        clearTimeout(game.timeoutHandler);
+        if (game.expectedIndex === game.currentCount) {
+            MoveForward();
+        } else {
+            game.expectedIndex++;
+            waitForInputSeries();
+        }
+    }
+}
+
+function MoveForward() {
+    game.currentCount++;
+    if (game.currentCount === 20) {
+        // you win
+
+    } else {
+        toggleButtons(false);
+        game.expectedIndex = 0;
+        playCurrentSeries(0);
+    }
 }
 
 function generateSeries() {
@@ -44,39 +72,41 @@ function waitForInputSeries() {
 function timeoutOrFailedHandler() {
     toggleButtons(false);
     playAudio(0, 0.8);
-    setTimeout(()=> {
-        playCurrentSeries();
+    setTimeout(() => {
+        playCurrentSeries(0);
     }, 800);
 }
 
 function startGame() {
-    playCurrentSeries();
+    game = Object.assign({}, gameInit);
+    generateSeries();
+    playCurrentSeries(0);
 }
 
 function playAudio(num, sec) {
     audios[num].load();
     audios[num].play();
-    setTimeout(function() {
+    setTimeout(function () {
         audios[num].pause();
-    }, sec*1000);
+    }, sec * 1000);
 }
 
-function playCurrentSeries() {
-    for (var i = 0; i <= game.currentCount; i++) {
-        var index = i;
-        var num = game.series[i];
-        setTimeout(() => {
-            $("#button" + num)[0].classList.add("light");
-            playAudio(num, 1);
-        }, 1000);
+function playCurrentSeries(index) {
+    var num = game.series[index];
+    setTimeout(() => {
+        $("#button" + num)[0].classList.add("light");
+        playAudio(num, 1);
+
         setTimeout(() => {
             $("#button" + num)[0].classList.remove("light");
             if (index === game.currentCount) {
                 toggleButtons(true);
                 waitForInputSeries();
+            } else {
+                playCurrentSeries(index+1);
             }
-        }, 2000);
-    }    
+        }, 1000);
+    }, 1000);
 }
 
 function toggleButtons(enable) {
